cfe4d31e1292efe4718635672962f9deb94628b6
the0xcollective <tommywilczek@utexas.edu>
2021-10-08 12:27:32 -0500

    Added three.js to react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import Home from './components/Home'
+import NavBar from './components/NavBar'
+
+
+export default function App() {
+  return (
+    <div>
+      <NavBar />
+      <Home />
+    </div>
+  );
+}
+
---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class NavBar extends Component {
+
+    render() {
+        return(
+            <p>I'M A NAV BAR!</p>
+        );
+    }
+}
